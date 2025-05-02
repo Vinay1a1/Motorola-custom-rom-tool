f93637554fadeb0a9879696385d37d6e5f1c8b5b
Vinay1a1 <109431786+Vinay1a1@users.noreply.github.com>
2025-05-02 13:41:01 +0530

    Update adb-set-path.bat

--- adb-set-path.bat
+++ adb-set-path.bat
@@ -1,4 +1,4 @@
 echo This will set platform tools to path so that can call it from anywhere. 
 
-curl -o platform-tools.zip
+curl -o platform-tools.zip https://dl.google.com/android/repository/platform-tools-latest-windows.zip
 
