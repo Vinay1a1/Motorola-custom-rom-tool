b32799d073390da06056ff841da32e5c7be17a6a
Vinay1a1 <109431786+Vinay1a1@users.noreply.github.com>
2025-05-02 14:00:45 +0530

    Update adb-set-path.bat

--- adb-set-path.bat
+++ adb-set-path.bat
@@ -1,4 +1,34 @@
 echo This will set platform tools to path so that can call it from anywhere. 
 
-curl -o platform-tools.zip https://dl.google.com/android/repository/platform-tools-latest-windows.zip
+echo Checking for admin privileges.
 
+net session
+If %ERRORLEVEL% neq 0 (
+echo Run this script as an administrator 
+pause)
+goto continue
+
+echo Running as admin
+
+echo Wait. Downloading platform tools.
+
+:continue
+curl -o platform-tools.zip https://dl.google.com/android/repository/platform-tools-latest-windows.zip
+echo Unzipping
+
+powershell -Command "Expand-Archive platform-tools.zip '%~dp0'"
+
+echo Creating a folder named platform tools in %USERPROFILE%
+
+mkdir "%USERPROFILE\platform-tools"
+
+echo copying files
+xcopy platform-tools "%USERPROFILE\platform-tools"
+
+echo setting it to path
+
+setx PATH "%USERPROFILE\platform-tools";%PATH%" /M
+
+echo Done. Now open run.bat
+
+pause
